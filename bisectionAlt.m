5fe09e4aa8058d8e691321db2d43aff9a4ddad13
axytho <jojojo.jonas888@hotmail.com>
2019-05-22 12:03:04 +0200

    it works

--- bisectionAlt.m
+++ bisectionAlt.m
@@ -1,29 +1,38 @@
-function [eigenvalues] = bisectionAlt(polynomials,a, b, maxEigenvalues, tol)
+function [eigenvalues] = bisectionAlt(A,a, b, maxEigenvalues, tol)
 %Check the number of sign changes in a and b
-if  (signchanges(polynomials,b) - signchanges(polynomials, a))<=0%Should not be less than 0, but can happen
+if  (signchanges(A,b) - signchanges(A, a))<=0%Should not be less than 0, but can happen
     eigenvalues = [];
 elseif (b-a)<tol
     eigenvalues = (a+b)/2; %if multiple eigenvalues within same tolerance, assume identical
 else
-    eigenA = bisectionAlt(polynomials,a, (a+b)/2, maxEigenvalues, tol);
+    eigenA = bisectionAlt(A,a, (a+b)/2, maxEigenvalues, tol);
     if size(eigenA, 1) < maxEigenvalues
-        eigenvalues = [eigenA, bisectionAlt(polynomials,(a+b)/2, b, maxEigenvalues, tol)];
+        eigenvalues = [eigenA, bisectionAlt(A,(a+b)/2, b, maxEigenvalues, tol)];
     else
         eigenvalues = eigenA;
     end 
 end
 end
-function numberOfSignChanges= signchanges(polynomials, x)
-oldValue = 1; %We start at one
-numberOfSignChanges = 0;
-for k=1:length(polynomials)
-   polynom = polynomials{k};
-   newValue = polyval(polynom, x);
-   if sign(newValue) ~= sign(oldValue)
-       if sign(newValue) ~=0
-           numberOfSignChanges = numberOfSignChanges + 1;
-       end
-   end
-   oldValue = newValue;
-end
+function numberOfSignChanges= signchanges(A, x)
+% First we calculate al the characteristic polynomials.
+    functionk2 = 0;
+    functionk1 = 1;
+
+    numberOfSignChanges= 0;
+    for k = 1:size(A,1)
+        if k > 1
+            sign(functionk0) ~= sign(functionk1)
+            sign(functionk0) ~= 0
+            functionk0 = (A(k, k) - x).* functionk1 - A(k, k-1)^2 .* functionk2;
+            
+        else
+            functionk0 =(A(k, k) - x);
+        end 
+        if (sign(functionk0) ~= sign(functionk1)) && (sign(functionk0) ~= 0)
+            numberOfSignChanges = numberOfSignChanges + 1;
+        end
+        functionk2 = functionk1;
+        functionk1 = functionk0;
+    end
+value = functionk0;
 end
--- matrixPolynomial.m
+++ matrixPolynomial.m
@@ -1,7 +1,18 @@
-function [outputArg1,outputArg2] = matrixPolynomial(inputArg1,inputArg2)
-%MATRIXPOLYNOMIAL Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+function value = matrixPolynomial(A,x)
+% First we calculate al the characteristic polynomials.
+    functionk2 = 0;
+    functionk1 = 1;
+
+    
+    for k = 1:size(A,1)
+        if k > 1
+            functionk0 = (A(k, k) - x).* functionk1 - A(k, k-1)^2 .* functionk2;    
+        else
+            functionk0 = (A(k, k) - x);
+        end 
+        functionk2 = functionk1;
+        functionk1 = functionk0;
+    end
+value = functionk0;
 end
 
