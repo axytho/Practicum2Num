a0968ae78f62c6a1e84eaa4b8f9068c66252eebe
Dennis Debree <dennis.debree@telenet.be>
2019-05-22 19:24:57 +0200

    Updated arnoldi plots with more relevant data en equal axis.

--- testArnoldi.m
+++ testArnoldi.m
@@ -1,5 +1,5 @@
-m = 100;
-A = sprand(m, m, 0.01);
+m = 1000;
+A = sprand(m, m, 0.2);
 b = rand(m , 1);
 v = b/norm(b);
 x = 1:m;
@@ -8,22 +8,22 @@ y = zeros(m, 1);
 figure(1)
 [V, H] = Arnoldi(A,v, 5);
 eigH = eig(H);
-eigA = eigs(A)
+eigA = eigs(A,100)
 plot(real(eigA), imag(eigA), 'r*',real(eigH), imag(eigH), 'bo')
 figure(2)
 [V, H] = Arnoldi(A,v, 10);
 eigH = eig(H);
-eigA = eigs(A);
+eigA = eigs(A,100);
 plot(real(eigA), imag(eigA), 'r*',real(eigH), imag(eigH), 'bo')
 figure(3)
 [V, H] = Arnoldi(A,v, 20);
 eigH = eig(H)
-eigA = eigs(A);
+eigA = eigs(A,100);
 plot(real(eigA), imag(eigA), 'r*',real(eigH), imag(eigH), 'bo')
 figure(4)
 [V, H] = Arnoldi(A,v, 100);
 eigH = eig(H);
-eigA = eigs(A);
+eigA = eigs(A,100);
 plot(real(eigA), imag(eigA), 'r*',real(eigH), imag(eigH), 'bo')
 
 
